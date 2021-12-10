f95b8a8947eb1d5e77a45abe314e73c35bbe920d
brentmitch <trekbrent@gmail.com>
2021-12-10 18:21:58 -0500

    Add waves to bottom of hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,6 +4,52 @@ import { Box, Container, Typography } from '@mui/material';
 
 import HeroSearchInput from '../hero-search-input/hero-search-input';
 
+import { styled } from '@mui/system';
+
+const Wave = styled('div')({
+	fontWeight: 'bold',
+	border: '1px sold black',
+	position: 'absolute',
+	left: '0',
+	bottom: '0',
+	right: '0',
+	'&:before': {
+		content: '""',
+		position: 'absolute',
+		left: '0',
+		bottom: '0',
+		right: '0',
+		backgroundRepeat: 'repeat',
+		height: '30px',
+		backgroundSize: '180px 30px',
+		backgroundImage:
+			'radial-gradient(ellipse at 90px 0px, transparent 90px, hsla(210, 2%, 99%, 1) 0px)',
+	},
+});
+
+const Wave50 = styled('div')({
+	fontWeight: 'bold',
+	border: '1px sold black',
+	position: 'absolute',
+	left: '0',
+	bottom: '0',
+	right: '0',
+	opacity: '0.5',
+	'&:before': {
+		content: '""',
+		position: 'absolute',
+		left: '0',
+		bottom: '0',
+		right: '0',
+		backgroundRepeat: 'repeat',
+		height: '20px',
+		backgroundPosition: 'bottom 0 right 180px',
+		backgroundSize: '180px 20px',
+		backgroundImage:
+			'radial-gradient(ellipse at 90px 0px, transparent 90px, hsla(210, 2%, 99%, 1) 0px)',
+	},
+});
+
 const Hero: React.FC<{}> = () => {
 	return (
 		<Box
@@ -13,10 +59,13 @@ const Hero: React.FC<{}> = () => {
 				marginBottom: 4,
 				paddingTop: '100px',
 				height: '380px',
+				position: 'relative',
 				background:
 					'linear-gradient(90deg, rgba(207,234,251,1) 0%, rgba(191,238,236,1) 100%)',
 			}}
 		>
+			<Wave />
+			<Wave50 />
 			<Container
 				sx={{
 					height: '100%',
